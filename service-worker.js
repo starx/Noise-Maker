6fd622d3de3608f8a759e35f51823f77e4ff1576
starx <swe_r3p0x9@mrnepal.com>
2025-04-23 15:32:52 +0100

    support base path

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,22 @@
+const BP = '.'; // BASE PATH // If PWA is served from a subfolder then update this
 const CACHE_NAME = 'noise-generator-cache-v1';
 const urlsToCache = [
-  './',
-  './index.html',
-  './ui.html',
-  './manifest.json',
-  './icons/icon-96.png',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
-  './css/style.css',
-  './js/app.js',
-  './js/noise.js',
-  './js/presets.js',
-  './js/white-noise-processor.js',
-  './js/pink-noise-processor.js',
-  './js/brown-noise-processor.js',
-  './vendor/framework7-bundle.min.css',
-  './vendor/framework7-bundle.min.js'
+  // `${BP}/`,
+  `${BP}/index.html`,
+  `${BP}/ui.html`,
+  `${BP}/manifest.json`,
+  `${BP}/icons/icon-96.png`,
+  `${BP}/icons/icon-192.png`,
+  `${BP}/icons/icon-512.png`,
+  `${BP}/css/style.css`,
+  `${BP}/js/app.js`,
+  `${BP}/js/noise.js`,
+  `${BP}/js/presets.js`,
+  `${BP}/js/white-noise-processor.js`,
+  `${BP}/js/pink-noise-processor.js`,
+  `${BP}/js/brown-noise-processor.js`,
+  `${BP}/vendor/framework7-bundle.min.css`,
+  `${BP}/vendor/framework7-bundle.min.js`,
 ];
 
 self.addEventListener('install', event => {
